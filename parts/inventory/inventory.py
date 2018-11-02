c8299c66402dc83426686d5096de403c767085d6
gjergjk71 <gjergjk71@gmail.com>
2018-11-02 22:59:07 +0100

    Created Item class

--- parts/inventory/inventory.py
+++ parts/inventory/inventory.py
@@ -1,7 +1,29 @@
 import pygame
 
+class Item:
+	def __init__(self,screen,inventory,x,y,w,h,item_image=None):
+		self.inventory = inventory
+		self.screen = screen
+		self.item_image = item_image
+		self.item_rect = pygame.Rect((x,y,w,h))
+		print(self.item_rect)
+		self.x = x
+		self.y = y
+		self.w = w
+		self.h = h
+	def blitme(self):
+		if self.item_image:
+			self.screen.blit(self.item_image,self.item_rect)
+
+	def handle_events(self,events):
+		for event in events:
+			if event.type == pygame.MOUSEBUTTONUP:
+				pos = pygame.mouse.get_pos()
+				if self.item_rect.collidepoint(pos) and self.inventory.state == "Open":
+					print("Clicked")
+
 class Inventory:
-	def __init__(self,screen,inventory_opened,inventory_closed,closeInventory_rect,slots,items,state="Closed"):
+	def __init__(self,screen,inventory_opened,inventory_closed,closeInventory_rect,items,slots,state="Closed"):
 		self.screen = screen
 		self.inventory_opened = inventory_opened
 		self.inventoryOpened_rect = inventory_opened.get_rect()
@@ -11,14 +33,34 @@ class Inventory:
 		self.slots = slots
 		self.items = items
 		self.state = state
-	def blitme(self):
+		self.slot_width =  11
+		self.slot_height =  12
+		self.slot_difference = 3
+		self.starting_x = 13
+		self.starting_y = 45
+		for i in range(self.slots[0]):
+			item_x = self.starting_x
+			item_y = self.starting_y
+			for s in range(i):
+				item_y += self.slot_height + self.slot_difference
+			for x in range(self.slots[1]):
+				for l in range(x):
+					item_x += self.slot_width + self.slot_difference
+				item = Item(self.screen,self,item_x,item_y,self.slot_width,self.slot_height)
+				self.items.append(item)
+	def blitme(self):	
 		if self.state == "Open":
 			self.screen.blit(self.inventory_opened,self.inventoryOpened_rect)
 		elif self.state == "Closed":
 			self.screen.blit(self.inventory_closed,self.inventoryClosed_rect)
+		for item in self.items:
+			item.blitme()
 	def handle_events(self,events):
 		for event in events:
 			self.open_close_inventory(event)
+	def handle_item_events(self,events):
+		for item in self.items:
+			item.handle_events(events)
 	def open_close_inventory(self,event):
 		if event.type == pygame.MOUSEBUTTONUP:
 			pos = pygame.mouse.get_pos()
